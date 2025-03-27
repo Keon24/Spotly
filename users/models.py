dcda454f432223f7c5bd3ad5b10d591cd207c259
Keon Powell <keonpowell@Keons-MacBook-Air.local>
2025-03-27 13:45:20 -0700

    user registered successfully will implement email verifiction next

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User (AbstractUser):
     email = models.EmailField(unique = True)
-    password = models.CharField(max_length = 50)
+    password = models.CharField(max_length =255)
     first_name = models.CharField(max_length =50)
     last_name = models.CharField(max_length =50)
 
